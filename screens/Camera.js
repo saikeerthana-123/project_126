cb4aed7f726ccc3c02019dd3d9ccb99b9086a2cc
saikeerthana-123 <63954676+saikeerthana-123@users.noreply.github.com>
2021-07-08 19:31:42 +0530

    Add files via upload

---
+++ screens/Camera.js
@@ -0,0 +1,97 @@
+// importing required libraries
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions'
+import { FontAwesome, Ionicons,MaterialCommunityIcons } from '@expo/vector-icons';
+
+//importing styles
+import styles from './styles';
+
+export default class CameraPage extends React.Component {
+    camera = null;
+
+    //setting initial stale of camera permissions
+    state = {
+        hasCameraPermission: null,
+    };
+
+    async componentDidMount() {
+        const camera = await Permissions.askAsync(Permissions.CAMERA);
+        const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+        const hasCameraPermission = (camera.status === 'granted' && audio.status === 'granted');
+        this.setState({ hasCameraPermission });
+    };
+
+    uploadImage = async (url) => {
+        //Check if any file is selected or not
+    
+          //If file selected then create FormData
+          const fileToUpload = url;
+          const data = new FormData();
+          data.append('name', 'Image Upload');
+          data.append('file_attachment', fileToUpload);
+          let res = await axios(
+            'https://548d65985c56.ngrok.io/predict-digit',
+            {
+              method: 'post',
+              headers: {
+                'Content-Type': 'multipart/form-data; ',
+              },
+              body: data,
+
+            }
+          );
+          let responseJson = await res.json();
+          if (responseJson.status == 1) {
+            alert('Upload Successful');
+          }
+        
+    };
+
+    //taking picture
+    takePicture = async () => {
+        if (this.camera) {
+          let photo = await this.camera.takePictureAsync();
+          var image=photo.uri
+          this.uploadImage(image)
+        }
+      }
+
+    render() {
+        const { hasCameraPermission } = this.state;
+
+        //permission granted
+        if (hasCameraPermission === null) {
+            return <View />;
+        //permission not granted
+        } else if (hasCameraPermission === false) {
+            return  <Text>Access to camera has been denied.</Text>;
+        } else {
+        return (
+            <View style={{flex:1,alignItems:'flex-end'}}>
+                
+                <Camera
+                    style={styles.preview}
+                    ref={ref => {this.camera = ref}}   
+                />
+                
+                <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                  onPress={()=>this.takePicture()}
+                  >
+                  <FontAwesome
+                      name="camera"
+                      style={{ color: "#fff", fontSize: 40}}
+                  />
+                </TouchableOpacity>
+            </View>
+        );
+        
+        };  
+    };
+};
